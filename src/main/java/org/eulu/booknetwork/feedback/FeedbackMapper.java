59d3a4479ce31759161766acc2509d11b57d540c
eulu <anegamega@gmail.com>
2024-09-15 21:35:27 +0300

    Implement find all feedback for the book

--- src/main/java/org/eulu/booknetwork/feedback/FeedbackMapper.java
+++ src/main/java/org/eulu/booknetwork/feedback/FeedbackMapper.java
@@ -3,6 +3,8 @@ package org.eulu.booknetwork.feedback;
 import org.eulu.booknetwork.book.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class FeedbackMapper {
     public Feedback toFeedback(FeedbackRequest request) {
@@ -14,6 +16,14 @@ public class FeedbackMapper {
                         .archived(false)
                         .shareable(false)
                         .build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer userId) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), userId))
                 .build();
     }
 }
--- src/main/java/org/eulu/booknetwork/feedback/FeedbackService.java
+++ src/main/java/org/eulu/booknetwork/feedback/FeedbackService.java
@@ -4,11 +4,16 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.eulu.booknetwork.book.Book;
 import org.eulu.booknetwork.book.BookRepository;
+import org.eulu.booknetwork.common.PageResponse;
 import org.eulu.booknetwork.exception.OperationNotPermittedException;
 import org.eulu.booknetwork.user.AppUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -32,5 +37,24 @@ public class FeedbackService {
 
         Feedback feedback = feedbackMapper.toFeedback(request);
         return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size);
+        AppUser user = (AppUser) connectedUser.getPrincipal();
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
     }
 }
